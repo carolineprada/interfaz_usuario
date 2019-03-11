05dd52c150baad0e245255f044adfb0c97b105f9
Caroline Prada <41764932+carolineprada@users.noreply.github.com>
2019-03-10 23:01:21 -0500

    Conexión

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$server = 'localhost';
+	$username = 'root';
+	$password = '';
+	$database = 'prueba';
+
+		
+	try 
+	{
+		$conn = new PDO ("mysql:host=$server; dbname=$database", $username, $password);
+		
+		echo "Conexión Exitosa";
+	} 
+	catch (PDOException $e)
+	{
+		echo "Error de Conexión" . $e->getMessage();
+	}
+?>
